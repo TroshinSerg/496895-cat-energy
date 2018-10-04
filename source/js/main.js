2bc39ce15830b894362efa80fb98bd69ec439533
serge-troshin <serge-troshin@yandex.ru>
2018-10-04 14:41:24 +0300

    Стилизовал секцию 'Живой пример' на главной, написал js для слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -5,3 +5,15 @@ menuToggle.addEventListener('click', function (evt) {
   menuToggle.classList.toggle('main-nav__toggle--opened');
   mainNavList.classList.toggle('main-nav__list--opened');
 });
+
+var exampleToggle = document.querySelector('.example__toggle'),
+    exampleImageWrapper = document.querySelectorAll('.example__image-wrapper');
+
+exampleToggle.addEventListener('click', function (evt) {
+  exampleToggle.classList.toggle('example__toggle--after');
+
+  for (var i = 0; i < exampleImageWrapper.length; i++) {
+    exampleImageWrapper[i].classList.toggle('example__image-wrapper--active');
+  }
+
+});
